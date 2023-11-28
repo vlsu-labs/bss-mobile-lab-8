c13107aa0476d48466f91514729e757480cb5b20
w1sp <semen2002bs@gmail.com>
2023-11-28 10:27:47 +0300

    init commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "bss-mobile-lab-8"
+include(":app")
+ 
